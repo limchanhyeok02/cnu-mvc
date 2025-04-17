84d0581508167c56397834330072351909323b89
limchanhyeok02 <imc35911@gmail.com>
2025-04-17 15:25:10 +0900

    Update MemberService.java

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -9,22 +9,23 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public Member join(Member member){
+    public Member join(Member member) {
+        if (memberRepository.findByEmail(member.getEmail()) != null) {
+            throw new IllegalStateException("이미 존재하는 이메일 계정입니다.");
+        }
         return memberRepository.save(member);
     }
 
-    public Member validateMember(String email, String pwd) {
-        Member findMember = findById(1L);
-        return findMember;
+    public Member validateMember(String email, String password) {
+        Member member = memberRepository.findByEmail(email);
+        if (member == null || !member.getPwd().equals(password)) {
+            throw new IllegalArgumentException("이메일 또는 비밀번호를 확인해주세요.");
+        }
+        return member;
     }
+
 
     public Member findById(Long id) {
         return memberRepository.findById(id);
     }
-
-    // 구현
-    public Member findByEmail(String email) {
-        return memberRepository.findByEmail(email);
-    }
-
 }
